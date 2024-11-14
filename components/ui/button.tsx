998b70cbbd71b1c53c5604408728a51b9de3ae43
BorisHrzenjak <fletlajn@gmail.com>
2024-11-14 19:39:57 +0100

    collor buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,19 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-[colors,opacity] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring  disabled:cursor-not-allowed disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-[colors,opacity] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow enabled:hover:bg-primary/90",
+          "bg-gradient-to-r from-purple-600 to-pink-600 text-white shadow hover:from-purple-500 hover:to-pink-500 active:from-purple-700 active:to-pink-700",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm enabled:hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm enabled:hover:bg-accent enabled:hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm enabled:hover:bg-accent enabled:hover:text-accent-foreground data-[state=active]:bg-gradient-to-r data-[state=active]:from-purple-600 data-[state=active]:to-pink-600 data-[state=active]:text-white",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm enabled:hover:bg-secondary/80",
-        ghost: "enabled:hover:bg-accent enabled:hover:text-accent-foreground",
+          "bg-secondary text-secondary-foreground shadow-sm enabled:hover:bg-secondary/80 data-[state=active]:bg-gradient-to-r data-[state=active]:from-purple-600 data-[state=active]:to-pink-600 data-[state=active]:text-white",
+        ghost: "enabled:hover:bg-accent enabled:hover:text-accent-foreground data-[state=active]:bg-gradient-to-r data-[state=active]:from-purple-600 data-[state=active]:to-pink-600 data-[state=active]:text-white",
         link: "text-primary underline-offset-4 enabled:hover:underline",
       },
       size: {
